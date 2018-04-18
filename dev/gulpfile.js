746194c77b5e6f97f99c598ab7c3f71c91fccf27
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2018-04-18 18:51:41 +0100

    Major reorganisation

---
+++ dev/gulpfile.js
@@ -0,0 +1,95 @@
+const gulp        = require('gulp');
+const gutil       = require( 'gulp-util' );
+const runSequence = require('run-sequence');
+const pump        = require('pump');
+
+/*------------------------------------------------------------------------------------------------*\
+    CSS    
+\*------------------------------------------------------------------------------------------------*/
+const sass   = require('gulp-sass');
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
+
+var css_src  = './build/';
+var css_dest = './build/css/';
+
+// Compile SCSS in expanded mode so it's easier to inspect the result.
+gulp.task('sass', (cb) =>
+    pump([
+        gulp.src(css_src + '**/*.scss'),
+        sass({outputStyle: 'expanded'}),
+        gulp.dest(css_dest)
+    ],
+    cb)
+);
+
+// Then create a minified version in the output folder.
+gulp.task('cssmin', (cb) =>
+    pump([
+        gulp.src(css_dest + '**/!(*.min)*.css'),
+        cssmin(),
+        rename({extname: '.min.css'}),
+        gulp.dest(css_dest)
+    ],
+    cb)
+);
+
+// This combined task makes it convenient to run all the steps together.
+gulp.task('css', () => {
+    console.log('Processing (S)CSS. See gulpfile.js for details.');
+    runSequence('sass', 'cssmin');
+})
+
+
+/*------------------------------------------------------------------------------------------------*\
+    FTP
+\*------------------------------------------------------------------------------------------------*/
+const ftp         = require( 'vinyl-ftp' );
+const ftpcrd      = require('./ftpcrd.json');
+
+
+var ftp_src_base = '..';
+var ftp_src      = [
+    ftp_src_base + '/dist/*',
+    '!css'
+];
+
+// Upload files
+gulp.task('ftp', () => {
+    console.log('Running FTP. See gulpfile.js for details.');
+
+    var conn = ftp.create( {
+        host:     ftpcrd.host,
+        user:     ftpcrd.user,
+        password: ftpcrd.pass,
+        parallel: 10,
+        log:      gutil.log
+    } );
+
+    return gulp.src( ftp_src, { base: ftp_src_base, buffer: false } )
+        //.pipe( conn.newer( '/public_html' ) ) // only upload newer files
+        .pipe( conn.dest(ftpcrd.dest) );
+});
+
+
+/*------------------------------------------------------------------------------------------------*\
+    WATCHERS
+\*------------------------------------------------------------------------------------------------*/
+
+// Watch CSS:
+gulp.task('watch_css', function(){
+    gulp.watch(css_src + '**/*.scss', ['css']); 
+});
+
+
+// Watch FTP:
+gulp.task('watch_ftp', function(){
+    gulp.watch(ftp_src, ['ftp']);
+});
+
+
+// Watch all of the above:
+gulp.task('watch_all', function(){
+    gulp.watch(css_src + '**/*.scss', ['css']);
+    gulp.watch(ftp_src, ['ftp']);
+});
